e665fe16e2a3877a78f03852da5791737eb99a3f
Michael Mitchell <michael@michaelmitchell.co.nz>
2017-02-03 05:19:15 +0200

    Fix?

--- system-tools/nginx-ingress/scripts/deploy.sh
+++ system-tools/nginx-ingress/scripts/deploy.sh
@@ -1,6 +1,8 @@
 #!/bin/bash
 DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 
+kubectl create configmap nginx-template --from-file=nginx.tmpl=../nginx.tmpl --namespace=system-tools
+
 kubectl apply \
   -f $DIR/../default-backend-controller.yaml \
   -f $DIR/../default-backend-service.yaml \
